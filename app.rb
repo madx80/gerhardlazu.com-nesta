853215f185cae658080ab9ce6c12bc5a58fa7643
Gerhard Lazu <gerhard@lazu.co.uk>
2010-04-07 12:57:29 +0100

    Replacing the old require with Bundler.require Didn't update the files under lib, don't think it's necessary

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
-require "rubygems"
-require "sinatra"
-require "builder"
-require "haml"
-require "sass"
+begin
+  # Try to require the preresolved locked set of gems.
+  require File.expand_path('../.bundle/environment', __FILE__)
+rescue LoadError
+  # Fall back on doing an unlocked resolve at runtime.
+  require "rubygems"
+  require "bundler"
+  Bundler.setup
+end
+
+Bundler.require
 
 require "lib/cache"
 require "lib/config"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,17 @@
+begin
+  # Try to require the preresolved locked set of gems.
+  require File.expand_path('../.bundle/environment', __FILE__)
+rescue LoadError
+  # Fall back on doing an unlocked resolve at runtime.
+  require "rubygems"
+  require "bundler"
+  Bundler.setup
+end
+
+Bundler.require(:default, :test)
+
 require "rubygems"
-require "spec"
 require "spec/interop/test"
-require "rack/test"
-require "rspec_hpricot_matchers"
-require "sinatra"
 
 Test::Unit::TestCase.send :include, Rack::Test::Methods
 
